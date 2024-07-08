3cdc8804c2f6c5955bd965d95f716aaaa3c3a75c
microsen <michael.j.rosen@gmail.com>
2024-07-08 15:50:19 -0500

    improve statusline

--- init.lua
+++ init.lua
@@ -170,9 +170,27 @@ require('lazy').setup({
       options = {
         icons_enabled = true,
         theme = 'nightfox',
-        component_separators = '|',
-        section_separators = '',
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
       },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { { 'filename', path = 1 }, 'searchcount' }, -- path = 1 shows relative path, path = 2 shows absolute path
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {}
+      },
+      tabline = {},
+      extensions = {}
     },
   },
 
