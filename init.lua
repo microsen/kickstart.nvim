3010e8fdc4e08a7d5cc10208a043a89ee72ba065
microsen <michael.j.rosen@gmail.com>
2024-07-24 11:54:40 +0530

    don't need all them grammars

--- init.lua
+++ init.lua
@@ -384,8 +384,7 @@ vim.keymap.set("n", "<leader>ln", ":set relativenumber!<CR>", { desc = "Use [N]o
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'ocaml', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc',
-    'vim' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'ocaml', 'rust', 'tsx', 'javascript', 'typescript' },
   sync_install = true,
   ignore_install = {},
   modules = {},
